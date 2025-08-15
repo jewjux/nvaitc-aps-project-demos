795e6b1aa4b0238128b8098529286ac04bf7c2c0
Darren Tan <darren@Darrens-MacBook-Air.local>
2025-08-15 15:54:18 +0800

    Update to Apache 2.0 license for open source release

--- projects/explorer-ai/chat_agent.py
+++ projects/explorer-ai/chat_agent.py
@@ -1,3 +1,17 @@
+# Copyright 2024 NVIDIA Corporation and Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain_community.chat_models import ChatOllama
 from langchain_community.tools.tavily_search import TavilySearchResults
